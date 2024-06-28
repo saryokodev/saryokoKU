2362c0aed1956d8744e93a021a4b56391234bc12
Saryoko <111623933+saryokodev@users.noreply.github.com>
2024-06-28 11:23:29 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,5 +6,10 @@ def main():
     st.markdown('# Rendering Markdown')
     st.write('Some Pythagorean Equation : ')
     st.latex('c^2 = a^2+b^2')
+    
+    house = 'house_clean.csv'
+    st.dataframe(house)
+    st.writer('metrics')
+    st.metric(label="Temperature", value="70 °F", delta="1.2 °F")
 if __name__ == '__main__':
     main()
