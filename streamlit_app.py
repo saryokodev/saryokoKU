1f8d671766dd9dfd02085da3e11ea8596bca6b67
Saryoko <111623933+saryokodev@users.noreply.github.com>
2024-06-28 13:39:06 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -16,6 +16,9 @@ def main():
     st.metric(label="Temperature", value="70 °F", delta="1.2 °F")
     st.write('Menampilkan DataFrame dengan St Agrid')
     AgGrid(house)
-    
+
+    st.table([x for x in range(1,5)])
+
+
 if __name__ == '__main__':
     main()
