91ac21550f520631ad28792d82864fc4f520aa4d
Saryoko <111623933+saryokodev@users.noreply.github.com>
2024-06-28 13:28:07 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,10 +1,11 @@
 import streamlit as st
 import pandas as pd
+from st_aggrid import AgGrid
 
 house = pd.read_csv('house_clean.csv')
 
 def main():
-    st.header('Halaman Utama Saryoko')
+    st.header('Halaman Streamlit Saryoko')
     st.subheader('This is SubHeader')
     st.markdown('# Rendering Markdown')
     st.write('Some Pythagorean Equation : ')
@@ -13,6 +14,7 @@ def main():
     st.dataframe(house)
     st.write('metrics')
     st.metric(label="Temperature", value="70 °F", delta="1.2 °F")
-
+    st.write('Menampilkan DataFrame dengan St Agrid')
+    agGrid(house)
 if __name__ == '__main__':
     main()
